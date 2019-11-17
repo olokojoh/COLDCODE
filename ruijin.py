1d3cfe086f6780992a3fd5cdee730ceab05a1ab0
olokojoh <54641961+olokojoh@users.noreply.github.com>
2019-11-17 16:50:33 -0500

    data_clean

--- ruijin.py
+++ ruijin.py
@@ -5,8 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('classic')
 dirpath = os.getcwd() 
-path2add = 'E:\GWU FALL\Data Mining\PROJECT'
-filepath = os.path.join( dirpath, path2add ,'data.csv')
+filepath = os.path.join( dirpath ,'data.csv')
 fifa = pd.read_csv(filepath)  
 fifa.head()
 
@@ -14,14 +13,11 @@ fifa.head()
 #To check whether index is unique and drop NA
 print('\n',fifa.head(),'\n')
 fifa.index
-fifa.index.is_unique
-dup=fifa.index.duplicated()
-print(dup)
-fifa_duo = fifa[dup]
-fifa_duo.shape
-fifa=fifa[dup == False]
-fifa = fifa.reset_index()
-fifa=fifa.dropna()
-fifa.head()
+print(fifa.index.is_unique)
+fifa.columns
+fifa = fifa.rename(columns={'Unnamed: 0': 'id', 'Club Logo':'Club_Logo', 'Preferred Foot':'Preferred_Foot', 'International Reputation':'International_Reputation', 'Weak Foot':'Weak_Foot', 'Skill Moves':'Skill_Moves', 'Work Rate':'Work_Rate', 'Body Type':'Body_Type', 'Real Face':'Real_Face', 'Jersey Number':'Jersey_Number', 'Loaned From':'Loaned_From', 'Contract Valid Until':'Contract_Valid_Until', 'Release Clause':'Release_Clause'})
+fifa = fifa.drop(columns=['Loaned_From'])
+fifa = fifa.dropna()
 
 # %%
+
